8fc02904ae0148e545672b6e7f8b4cbfcb44649f
Tdogb <tdogbeard@gmail.com>
2019-03-17 17:19:58 -0400

    working category images

--- catalogue/templatetags/category_render_tags.py
+++ catalogue/templatetags/category_render_tags.py
@@ -4,15 +4,21 @@ from django.template.loader import select_template
 from oscar.core.loading import get_model
 
 register = template.Library()
-Category = get_model('catalogue', 'category')
+# Category = get_model('catalogue', 'category')
 
 @register.simple_tag(takes_context=True)
-def render_category(context, category):
+def render_category(context, category, url):
     if not category:
         return ''
-    template_ = get_template('catalogue/partials/category.html')
+    # context['url'] = category.get_absolute_image_url()
+    names = 'catalogue/partials/category.html'
+    template_ = get_template(names)
     context = context.flatten()
 
     # Ensure the passed product is in the context as 'product'
     context['category'] = category
+    context['url'] = url
+
+    print("context['category']")
+    print(category)
     return template_.render(context)
