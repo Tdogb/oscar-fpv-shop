932ae9b8ba6806fcedbab6a20ac8c11c405525b5
Tdogb <tdogbeard@gmail.com>
2019-03-20 19:23:53 -0400

    media fixes 2

--- FirstShop/settings.py
+++ FirstShop/settings.py
@@ -203,7 +203,6 @@ AWS_STORAGE_BUCKET_NAME = 'fpv-site-bucket'
 AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = os.environ['AWS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
-AWS_DEFAULT_ACL = None
 
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
