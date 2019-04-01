ebb0703dc8b92df8158064300064650fa996ff4c
Tdogb <tdogbeard@gmail.com>
2019-03-31 22:35:48 -0400

    styles

--- staticfiles/custom.js
+++ staticfiles/custom.js
@@ -1,14 +1,6 @@
-console.log("activated");
-
 $(".home-categories").bind("mouseenter mouseleave", function () {
     jQueryTest = jQuery(this).find("h3").text();
     targetElement = $(".product_detail:first-child:first-child:contains(" + jQueryTest + ")");
     targetElement.toggleClass("homehide", true);
     $(".product_detail:first-child:first-child").not(targetElement).toggleClass("homehide", false)
 });
-
-// $(".home-categories").bind("mouseout", function () {
-//     jQueryTest = jQuery(this).find("h3").text();
-//     targetElement = $(".product_detail:first-child:first-child:contains(" + jQueryTest + ")");
-//     targetElement.toggleClass("homehide", false);
-// });
